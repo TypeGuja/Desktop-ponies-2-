8678a41edf6ec053d31fd2457e7c6bdb329d81a6
Type_Guja <gusamogus84@yandex.ru>
2026-05-08 18:59:30 +0400

    Makefiles

--- target/debug/build/syn-c25fe31d3a31e49e/build_script_build-c25fe31d3a31e49e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Documents\GitHub\Desktop-ponies-2-\target\debug\build\syn-c25fe31d3a31e49e\build_script_build-c25fe31d3a31e49e.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
-
-C:\Users\user\Documents\GitHub\Desktop-ponies-2-\target\debug\build\syn-c25fe31d3a31e49e\build_script_build-c25fe31d3a31e49e.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
-
-C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs:
--- target/debug/build/thiserror-7d2cae6c91fe779d/build_script_build-7d2cae6c91fe779d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Documents\GitHub\Desktop-ponies-2-\target\debug\build\thiserror-7d2cae6c91fe779d\build_script_build-7d2cae6c91fe779d.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs
-
-C:\Users\user\Documents\GitHub\Desktop-ponies-2-\target\debug\build\thiserror-7d2cae6c91fe779d\build_script_build-7d2cae6c91fe779d.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs
-
-C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs:
