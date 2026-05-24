9c59a4bfca1d1207839ece086d56bc974f942e00
Type_Guja <gusamogus84@yandex.ru>
2026-05-24 10:03:54 +0400

    build

--- build.rs
+++ build.rs
@@ -3,4 +3,5 @@ fn main() {
     println!("cargo:rustc-link-lib=advapi32");
     println!("cargo:rustc-link-lib=ole32");
     println!("cargo:rustc-link-lib=user32");
+    println!("cargo:rerun-if-changed=/src_uiEditor/");
 }
